cbbe41524cabc55a3e0ddf36fc6318a476a9bfe1
Ignacio Nuñez Valcarce <ignacionunezvalcarce@gmail.com>
2026-04-29 22:55:31 -0300

    tamaño de la canción en RevealScreen

--- components/RevealScreen.tsx
+++ components/RevealScreen.tsx
@@ -70,58 +70,6 @@ export default function RevealScreen() {
       }}
     >
       <TeamScores />
-
-      {/* CARD DE LA CANCIÓN */}
-      <div
-        style={{
-          marginTop: 40,
-          background: '#46B5F0',
-          borderRadius: 28,
-          padding: '38px 24px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: 320,
-        }}
-      >
-        <p
-          style={{
-            fontSize: '1.15rem',
-            fontWeight: 700,
-            color: 'white',
-            marginBottom: 18,
-            textAlign: 'center',
-          }}
-        >
-          {currentSong.artist}
-        </p>
-
-        <h1
-          style={{
-            fontSize: '5.2rem',
-            fontWeight: 900,
-            lineHeight: 1,
-            color: 'white',
-            marginBottom: 24,
-          }}
-        >
-          {currentSong.year}
-        </h1>
-
-        <p
-          style={{
-            fontSize: '1.2rem',
-            fontWeight: 600,
-            color: 'white',
-            textAlign: 'center',
-          }}
-        >
-          {currentSong.title}
-        </p>
-      </div>
-
-      {/* RESULTADO */}
       <div
         style={{
           marginTop: 26,
@@ -172,6 +120,57 @@ export default function RevealScreen() {
           </p>
         )}
       </div>
+      {/* CARD DE LA CANCIÓN */}
+      <div
+        style={{
+          marginTop: 40,
+          background: '#46B5F0',
+          borderRadius: 28,
+          padding: '28px 24px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: 180,
+        }}
+      >
+        <p
+          style={{
+            fontSize: '1.15rem',
+            fontWeight: 700,
+            color: 'white',
+            marginBottom: 18,
+            textAlign: 'center',
+          }}
+        >
+          {currentSong.artist}
+        </p>
+
+        <h1
+          style={{
+            fontSize: '5.2rem',
+            fontWeight: 900,
+            lineHeight: 1,
+            color: 'white',
+            marginBottom: 24,
+          }}
+        >
+          {currentSong.year}
+        </h1>
+
+        <p
+          style={{
+            fontSize: '1.2rem',
+            fontWeight: 600,
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          {currentSong.title}
+        </p>
+      </div>
+
+      {/* RESULTADO */}
 
       {/* BOTÓN */}
       <div
